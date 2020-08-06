e22fbc94bf652255c7b99d73c4b5d29cccf3d41b
Rawit Panjaroen <check.rawit@gmail.com>
2020-08-06 21:05:02 +0700

    feat: add route for read all face results of latest image

--- app/routes/images.py
+++ app/routes/images.py
@@ -8,6 +8,39 @@ from app.utils import image_to_data_uri
 
 router = APIRouter()
 
+SELECT_ALL_FACES_RESULT_QUERY = """
+SELECT 
+    face.id as id,
+    face.position_top,
+    face.position_right,
+    face.position_bottom, 
+    face.position_left,
+    gender.male_confidence,
+    gender.female_confidence,
+    age.0_to_10_confidence,
+    age.11_to_20_confidence,
+    age.21_to_30_confidence,
+    age.31_to_40_confidence,
+    age.41_to_50_confidence,
+    age.51_to_60_confidence,
+    age.61_to_70_confidence,
+    age.71_to_100_confidence,
+    emotion.uncertain_confidence,
+    emotion.angry_confidence,
+    emotion.disgusted_confidence,
+    emotion.fearful_confidence,
+    emotion.happy_confidence,
+    emotion.neutral_confidence,
+    emotion.sad_confidence,
+    emotion.surprised_confidence,
+    face_recognition.label
+FROM face 
+    INNER JOIN gender ON face.gender_id = gender.id
+    INNER JOIN age ON face.age_id = age.id
+    INNER JOIN emotion ON face.emotion_id = emotion.id
+    INNER JOIN face_recognition ON face.face_recognition_id = face_recognition.id
+WHERE face.image_id=%(image_id)s;
+"""
 
 def fetch_latest_image(cnx: pymysql.connections.Connection):
     # Get DictCursor
@@ -27,10 +60,14 @@ def read_all_faces_latest_image():
 
     latest_image = fetch_latest_image(sql_connection)
 
+    with sql_connection.cursor(cursor=pymysql.cursors.DictCursor) as cursor:
+        cursor.execute(SELECT_ALL_FACES_RESULT_QUERY, {'image_id': latest_image['id']})
+        faces = cursor.fetchall()
+
     # Close database connection
     sql_connection.close()
 
-    return {} # TODO: return all result
+    return faces if faces is not None else []
 
 
 @router.get("/latest")
